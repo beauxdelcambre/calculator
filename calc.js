002d6ac7ee0dd2908eeaad0458f96fd84614783e
beauxdelcambre <beaux@stingphotodesign.com>
2021-10-14 12:38:33 -0500

    initial commit

--- calc.js
+++ calc.js
@@ -14,25 +14,30 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
-	
+
 
 	if (mathSymbol === "+"){
 		console.log(num1 + num2)
+	}
 	
-    if (mathSymbol === "-")
-	console.log(num1 - num2)
-
-	if (mathSymbol === "*")
-	console.log(num1 * num2)
-
-	if (mathSymbol === "/")
-	console.log(num1 / num2)
-
+    if (mathSymbol === "-"){
+	console.log(num1 - num2);
+	}
+	if (mathSymbol === "*"){
+	console.log(num1 * num2);
+	}
+	if (mathSymbol === "/"){
+	console.log(num1 / num2);
+	}
+	if (mathSymbol === "sqrt"){
+		console.log(Math.sqrt(num1));
 	}
 
-console.log('mathSymbol', mathSymbol)
-console.log('num1' , num1)
-console.log('num2', num2)
+	
+
+//console.log('mathSymbol', mathSymbol)
+//console.log('num1' , num1)
+//console.log('num2', num2)
 
 
 
